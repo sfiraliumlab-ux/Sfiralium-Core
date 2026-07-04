ff4da76e7352ff5de2e747f44079f46bd719328c
Станислав <sfiralium.lab@gmail.com>
2026-07-04 20:51:16 +0300

    Create __init__.py

---
+++ src/sfiral/__init__.py
@@ -0,0 +1,9 @@
+"""
+Sfiral-Core: Topological Computing Framework
+Реализация принципа Сфирали: зеркальная антисимметрия, S-петля и фрактальная вложенность.
+"""
+from .neurons import SINLayer, FSINLayer
+from .geometry import H2Algebra, H3Algebra
+
+__version__ = "0.1.0"
+__author__ = "O.S. Basargin, S. Chernenko"
